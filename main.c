0444c9b88a478af638acf51fd8a191d7d7bf17a4
RoeYeoR <157602723+RoeYeoR@users.noreply.github.com>
2024-02-05 23:42:12 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,48 +2,42 @@
 #include <stdio.h>
 
 int main() {
+
     int graph[SIZE][SIZE];
-
-   
-
+    int i, j;
     char action;
 
     do {
-        printf("Choose an action (A, B, C, D): ");
-        scanf(" %c", &action);  // Note the space before %c to consume the newline character
+        
+        scanf(" %c", &action);  
 
         switch (action) {
             case 'A':
-                printf("Enter the matrix values (10x10):\n");
+                
                 inputMatrix(graph);
 
                 break;
 
             case 'B':
-                 int i, j;
-                printf("Enter i and j to check if there is a path: ");
+
                 scanf("%d %d", &i, &j);
-                printf("Path from %d to %d: %s\n", i, j, isValidPath(graph, i, j) ? "True" : "False");
+                printf(isValidPath(graph, i, j) ? "True" : "False");
                 break;
 
             case 'C':
-                printf("Enter i and j to find the shortest path: ");
+                
                 scanf("%d %d", &i, &j);
                 int shortest = shortestPath(graph, i, j);
-                if (shortest == -1) {
-                printf("No path exists.\n");
-                } else {
-                printf("Shortest path from %d to %d: %d\n", i, j, shortest);
-                }
+                printf("%d", shortest);
+               
                 break;
 
             case 'D':
-                // Exit the program
-                printf("Exiting the program...\n");
+                
                 break;
 
             default:
-                printf("Invalid action. Please choose A, B, C, or D.\n");
+            
                 break;
         }
     } while (action != 'D');
